8c3591a09aef566934909959b23a4a0efb3e71a4
AsaeNeopie <n.noahchery@gmail.com>
2024-03-19 11:22:14 +0100

    Plateforme

--- Assets/Script/PlayerBehaviour.cs
+++ Assets/Script/PlayerBehaviour.cs
@@ -1,30 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerBehaviour : MonoBehaviour
 {
     [SerializeField] float vitesseRotation;
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        Move();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.Rotate(Vector3.right * vitesseRotation*Time.deltaTime);
+            transform.Rotate(Vector3.right * vitesseRotation * Time.deltaTime);
         }
 
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
             transform.Rotate(Vector3.left * vitesseRotation * Time.deltaTime);
         }
 
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(Vector3.back * vitesseRotation * Time.deltaTime);
         }
 
-        if(Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward * vitesseRotation * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.up * vitesseRotation * Time.deltaTime);
+        }
+
+        if(Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.down *  vitesseRotation * Time.deltaTime);
         }
     }
 }
