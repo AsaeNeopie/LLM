2c7dade4fa7123a48892284f7bec386d45619637
AsaeNeopie <n.noahchery@gmail.com>
2024-03-22 14:04:09 +0100

    level 2

--- Assets/Script/RestartLevel.cs
+++ Assets/Script/RestartLevel.cs
@@ -17,5 +17,36 @@ public class RestartLevel : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
     }
+
+    public void NextScene1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void NextScene2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void NextScene3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void NextScene4()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void NextScene5()
+    {
+        SceneManager.LoadScene("Level5");
+    }
+
+    public void NextScene6()
+    {
+        SceneManager.LoadScene("Level6");
+    }
+
 }
 
