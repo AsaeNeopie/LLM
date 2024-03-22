4d14a62cfc67d7bba53883b283f9bbee85bd9e30
AsaeNeopie <n.noahchery@gmail.com>
2024-03-22 14:19:36 +0100

    Entames level 3 plus reparation qu'elle que bug

--- Assets/Script/RestartLevel.cs
+++ Assets/Script/RestartLevel.cs
@@ -20,32 +20,32 @@ public class RestartLevel : MonoBehaviour
 
     public void NextScene1()
     {
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("Level 1");
     }
 
     public void NextScene2()
     {
-        SceneManager.LoadScene("Level2");
+        SceneManager.LoadScene("Level 2");
     }
 
     public void NextScene3()
     {
-        SceneManager.LoadScene("Level3");
+        SceneManager.LoadScene("Level 3");
     }
 
     public void NextScene4()
     {
-        SceneManager.LoadScene("Level4");
+        SceneManager.LoadScene("Level 4");
     }
 
     public void NextScene5()
     {
-        SceneManager.LoadScene("Level5");
+        SceneManager.LoadScene("Level 5");
     }
 
     public void NextScene6()
     {
-        SceneManager.LoadScene("Level6");
+        SceneManager.LoadScene("Level 6");
     }
 
 }
